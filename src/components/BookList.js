8ee45f80ea02e42d3a025a6495e7f0070de87d19
Usaid <usaidpeerzada@gmail.com>
2020-07-25 23:24:58 +0530

    contextapi

---
+++ src/components/BookList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends Component {
+  render() {
+    return (
+    <ThemeContext.Consumer>{(context) => {
+      const { isLightTheme, light, dark } = context;
+      const theme = isLightTheme ? light : dark;
+      return (
+      <div className="book-list" style={{ color: theme.syntax ,background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }}>Booms</li>
+        <li style={{ background: theme.ui }}>Booms</li>
+        <li style={{ background: theme.ui }}>Booms</li>
+      </ul>
+      </div>
+    )
+  }}</ThemeContext.Consumer>
+  );
+}  
+}
+
+export default BookList;
